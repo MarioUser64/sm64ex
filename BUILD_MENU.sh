bd0490eeeb9bde4232119442d30cf441b5628b03
MarioUser64 <69533071+MarioUser64@users.noreply.github.com>
2020-08-16 05:37:41 +0200

    Update BUILD_MENU.sh

--- BUILD_MENU.sh
+++ BUILD_MENU.sh
@@ -1,56 +1,125 @@
 #!/bin/bash 
-title="SM64ex Build Menu"
-Prompt="Pick an option:" && echo ""
+title="
+SM64ex Build Menu
+"
+
+Prompt="
+Pick an option:
+"
 options=("us" "eu" "jp" "clean")
 
 clear
 echo "$title"
 echo ""
-PS3="$Prompt" && echo ""
+PS3="$Prompt"
 
 select opt in "${options[@]}" "Quit"; do 
 
     case "$REPLY" in
 
-    1 ) echo "Building $opt version"
-<<<<<<< HEAD
-	    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1 RENDER_API=D3D11
-=======
-	        make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
->>>>>>> 778d8972c2d94efef13b69fe2215164b0df137ce
+    1 ) echo ""
+		echo "Use OpenGL Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
+		else
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1 RENDER_API=D3D11
+        fi
 		./doxygen/shortcut.exe $opt
+		echo ""
+		echo "Use HD Textures Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then 
+		FILE=doxygen/$opt"_hd.zip"
+		if [ -f "$FILE" ]; then
+			\cp "$FILE" build/$opt"_pc/res/gfx.zip"
+		fi
+		else 
+		FILE=build/$opt"_pc/res/gfx.zip"
+		if [ -f "$FILE" ]; then
+			rm build/$opt"_pc/res/gfx.zip"
+		fi
+        fi
+		echo ""
 		echo "Finish"
+		echo ""
 		read -n 1 -s -r -p "Press any key to continue"
 		clear
 	;;
-    2 ) echo "Building $opt version"
-<<<<<<< HEAD
-	    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
-=======
-	        make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
->>>>>>> 778d8972c2d94efef13b69fe2215164b0df137ce
+    2 ) echo ""
+		echo "Use OpenGL Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
+		else
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1 RENDER_API=D3D11
+        fi
 		./doxygen/shortcut.exe $opt
+		echo ""
+		echo "Use HD Textures Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then 
+		FILE=doxygen/$opt"_hd.zip"
+		if [ -f "$FILE" ]; then
+			\cp "$FILE" build/$opt"_pc/res/gfx.zip"
+		fi
+		else 
+		FILE=build/$opt"_pc/res/gfx.zip"
+		if [ -f "$FILE" ]; then
+			rm build/$opt"_pc/res/gfx.zip"
+		fi
+        fi
+		echo ""
 		echo "Finish"
+		echo ""
 		read -n 1 -s -r -p "Press any key to continue"
 		clear
 	;;
-    3 ) echo "Building $opt version"
-<<<<<<< HEAD
-	    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
+    3 ) echo ""
+		echo "Use OpenGL Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
+		else
+		    make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1 RENDER_API=D3D11
+        fi
 		./doxygen/shortcut.exe $opt
+		echo ""
+		echo "Use HD Textures Y/N"
+		echo ""
+        read response
+        if [[ "$response" == "Y" ]]
+		then 
+		FILE=doxygen/$opt"_hd.zip"
+		if [ -f "$FILE" ]; then
+			\cp "$FILE" build/$opt"_pc/res/gfx.zip"
+		fi
+		else 
+		FILE=build/$opt"_pc/res/gfx.zip"
+		if [ -f "$FILE" ]; then
+			rm build/$opt"_pc/res/gfx.zip"
+		fi
+        fi
+		echo ""
 		echo "Finish"
+		echo ""
 		read -n 1 -s -r -p "Press any key to continue"
 		clear
-=======
-	        make -j$(nproc) VERSION=$opt NODRAWINGDISTANCE=1 TEXTURE_FIX=1 EXTERNAL_DATA=1 EXT_OPTIONS_MENU=1
-	        ./doxygen/shortcut.exe $opt
-	        echo "Finish"
-	        read -n 1 -s -r -p "Press any key to continue"
-	        clear
->>>>>>> 778d8972c2d94efef13b69fe2215164b0df137ce
 	;;
-    4 ) make $opt
-	        echo "Finish"
+    4 ) echo ""
+	    echo "Cleaning project"
+	    make $opt
+	    echo "Finish"
 		read -n 1 -s -r -p "Press any key to continue"
 		clear
 	;;
